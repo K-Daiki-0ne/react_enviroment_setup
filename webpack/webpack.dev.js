cc1c4b1b704a6d56e9a2bd9f868f9d21047f7e9a
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-05-18 00:01:58 +0900

    add: webpack plugins for dev and prod

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,8 +1,16 @@
+const webpack = require('webpack');
+
 module.exports = {
   // ログの出力を可能にする
   mode: 'development',
 
   // バンドル後とバンドル前のソースコードの紐付けを行う
   // ブラウザのコンソールでエラーの確認を行うことができるようにする
-  devtool: 'cheap-module-source-map'
+  devtool: 'cheap-module-source-map',
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Vishwas')
+    })
+  ]
 }
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,9 +1,16 @@
 // 本番環境用のWebpackの設定コード
+const webpack = require('webpack');
 
 module.exports = {
   // ログの出力を不可とする
   mode: 'production',
 
   // 本番環境用の高品質ソースマップ
-  devtool: 'source-map'
+  devtool: 'source-map',
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Codevolution')
+    })
+  ]
 }
