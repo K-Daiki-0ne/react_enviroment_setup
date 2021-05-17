29fb22fd3431fc3c806ae3899adbf7a60104edf7
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-05-17 22:44:35 +0900

    create: iamge & svg file setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import './App.css'
+import './App.css';
+import IMAGE from './react.png';
 
 export default function App(){
-  return <h1>React enviroment setup</h1>
+  return ( 
+    <>
+      <h1>React enviroment setup</h1>
+      <img src={IMAGE} alt='React logo' width={300} height={300} />
+    </>
+  )
 }
