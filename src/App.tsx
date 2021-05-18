357bfa6a463bb337938f56db7b36e23fd7c0e847
_ 0ne <kouguchi3456@yahoo.co.jp>
2021-05-19 07:21:01 +0900

    modified: import path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 import IMAGE from './react.png';
+import { Counter } from './components/Counter';
 
 export default function App(){
   return ( 
     <>
       <h1>React enviroment setup</h1>
       <img src={IMAGE} alt='React logo' width={300} height={300} />
+      <Counter />
     </>
   )
 }
